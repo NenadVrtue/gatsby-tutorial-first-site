3eee92eccdcdbc41d616d760f721a368e23675c6
NenadVrtue <hello@vrtuedigital.com>
2021-06-19 14:06:05 +0200

    Changing image of a sky to tree

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
         <p>I'm making this by following the Gatsby Tutorial</p>
         <StaticImage
           alt='Fcking huge image'
-          src='../images/sky.jpg'
+          src='../images/tree.jpg'
         />
       </Layout>
   )
