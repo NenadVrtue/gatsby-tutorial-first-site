b9ecae797e45d6cc71a04ce44b7fe6e9af94cf2f
NenadVrtue <hello@vrtuedigital.com>
2021-06-19 13:32:05 +0200

    Tutorial Part 2 Layout and pages

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import {Link} from 'gatsby'
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+    return(
+        <Layout pageTitle="About me">
+        <p>Hi! I'm learning to build websites with Gatsby and React</p>
+
+        </Layout>
+        
+    )
+}
+
+export default AboutPage
